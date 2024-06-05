4a69d52ff213b0335f273d83b0220334812b3af7
ExeVirus <ddsallad@gmail.com>
2024-06-04 22:22:32 -0400

    Created Mr. Superdebugger

--- src/benchmark/benchmark_activeobjectmgr.cpp
+++ src/benchmark/benchmark_activeobjectmgr.cpp
@@ -37,7 +37,6 @@ inline v3f randpos()
 
 inline void fill(server::ActiveObjectMgr &mgr, size_t n)
 {
-	mgr.clear();
 	for (size_t i = 0; i < n; i++) {
 		auto obj = std::make_unique<TestObject>(randpos());
 		bool ok = mgr.registerObject(std::move(obj));
@@ -53,11 +52,13 @@ void benchGetObjectsInsideRadius(Catch::Benchmark::Chronometer &meter)
 	server::ActiveObjectMgr mgr;
 	size_t x;
 	std::vector<ServerActiveObject*> result;
+	mysrand(2010112); // keep the test identical for comparing perf changes
 
 	auto cb = [&x] (ServerActiveObject *obj) -> bool {
 		x += obj->m_static_exists ? 0 : 1;
 		return false;
 	};
+	mgr.clear();
 	fill(mgr, N);
 	meter.measure([&] {
 		x = 0;
@@ -75,11 +76,13 @@ void benchGetObjectsInArea(Catch::Benchmark::Chronometer &meter)
 	server::ActiveObjectMgr mgr;
 	size_t x;
 	std::vector<ServerActiveObject*> result;
+	mysrand(2010112); // keep the test identical for comparing perf changes
 
 	auto cb = [&x] (ServerActiveObject *obj) -> bool {
 		x += obj->m_static_exists ? 0 : 1;
 		return false;
 	};
+	mgr.clear();
 	fill(mgr, N);
 	meter.measure([&] {
 		x = 0;
@@ -94,6 +97,67 @@ void benchGetObjectsInArea(Catch::Benchmark::Chronometer &meter)
 	mgr.clear(); // implementation expects this
 }
 
+void benchPseudorandom(Catch::Benchmark::Chronometer &meter)
+{
+	server::ActiveObjectMgr mgr;
+	std::vector<ServerActiveObject*> result;
+	result.reserve(3000); // don't want mem resizing to be a factor in tests
+	mysrand(2010112); // keep the test identical for comparing perf changes
+
+	auto iterationManipulator = [&mgr] (ServerActiveObject *obj) -> bool {
+		if (obj == nullptr)	return false;
+		int val = myrand_range(1, 80); // seldom, just remember this is an expensive callback
+		if( val == 1) {
+			mgr.removeObject(obj->getId());
+			return false;
+		} else if ( val == 2) {
+			fill(mgr, 1);
+		}
+		return true;
+	};
+
+	auto inArea = [&result, &iterationManipulator] (server::ActiveObjectMgr* mgr) {
+		result.clear();
+		v3f pos = randpos();
+		v3f off(50, 50, 50);
+		off[myrand_range(0, 2)] = 10;
+		mgr->getObjectsInArea({pos, pos + off}, result, iterationManipulator);
+		return result.size();
+	};
+
+	auto inRadius = [&result, &iterationManipulator] (server::ActiveObjectMgr* mgr) {
+		result.clear();
+		mgr->getObjectsInsideRadius(randpos(), 30.0f, result, iterationManipulator);
+		return result.size();
+	};
+	
+	meter.measure([&] {
+		fill(mgr, 1000); // start with 1000 to keep things messy
+
+		// Psuedorandom object manipulations
+		// Note that the inArea and inRadius checks will manipulate the number of objects
+		for (int i = 0; i < 200; i++) {
+			switch(myrand_range(0,2)) {
+				case 0: // move three objects
+					//mgr.getActiveObject(1)->setBasePosition(randpos());
+					//mgr.getActiveObject(2)->setBasePosition(randpos());
+					//mgr.getActiveObject(3)->setBasePosition(randpos());
+					break;
+				case 1:
+					inArea(&mgr);
+					break;
+				default:
+					inRadius(&mgr);
+			}
+		}
+		WARN("inArea  :" + std::to_string(inArea(&mgr)  )); // for validation against new implementations
+		WARN("inRadius:" + std::to_string(inRadius(&mgr)));
+		mgr.clear();
+	});
+
+	mgr.clear(); // implementation expects this
+}
+
 #define BENCH_INSIDE_RADIUS(_count) \
 	BENCHMARK_ADVANCED("inside_radius_" #_count)(Catch::Benchmark::Chronometer meter) \
 	{ benchGetObjectsInsideRadius<_count>(meter); };
@@ -102,10 +166,18 @@ void benchGetObjectsInArea(Catch::Benchmark::Chronometer &meter)
 	BENCHMARK_ADVANCED("in_area_" #_count)(Catch::Benchmark::Chronometer meter) \
 	{ benchGetObjectsInArea<_count>(meter); };
 
-TEST_CASE("ActiveObjectMgr") {
-	BENCH_INSIDE_RADIUS(200)
-	BENCH_INSIDE_RADIUS(1450)
+#define BENCH_PSEUDORANDOM() \
+	BENCHMARK_ADVANCED("psuedorandom")(Catch::Benchmark::Chronometer meter) \
+	{ benchPseudorandom(meter); };
 
-	BENCH_IN_AREA(200)
-	BENCH_IN_AREA(1450)
+TEST_CASE("ActiveObjectMgr") {
+	//BENCH_INSIDE_RADIUS(200)
+	//BENCH_INSIDE_RADIUS(1450)
+	//BENCH_INSIDE_RADIUS(10000)
+
+	//BENCH_IN_AREA(200)
+	//BENCH_IN_AREA(1450)
+	//BENCH_IN_AREA(10000)
+
+	BENCH_PSEUDORANDOM()
 }
